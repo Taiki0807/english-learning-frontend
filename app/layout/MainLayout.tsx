7dadaff26f71b6086ab87b43d323588fec44c739
Taiki0807 <hosokawataiki265@gmail.com>
2023-05-17 19:09:52 +0900

    feat: add main layout

---
+++ app/layout/MainLayout.tsx
@@ -0,0 +1,14 @@
+import { Header } from '../components/pages';
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+}
