773dfdb6f4b90ac280c5399518fee3e6ac348e31
Taiki0807 <hosokawataiki265@gmail.com>
2023-05-19 01:04:11 +0900

    fix: add missing import '@testing-library/jest-dom/extend-expect'

--- app/components/features/Toast/Toast.test.tsx
+++ app/components/features/Toast/Toast.test.tsx
@@ -1,6 +1,7 @@
 import { composeStories } from '@storybook/react';
 import { render, screen } from '@testing-library/react';
 import * as stories from './Toast.stories';
+import '@testing-library/jest-dom/extend-expect';
 
 const { Default } = composeStories(stories);
 
