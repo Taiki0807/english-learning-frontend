097d7c72f2ea4a161661dc61ff250d9fc1ae9d5f
Taiki0807 <hosokawataiki265@gmail.com>
2023-05-19 00:43:59 +0900

    feat: grouped routes using Route Groups

--- app/about/page.tsx
+++
@@ -1,7 +0,0 @@
-import MainLayout from '../layout/MainLayout';
-
-const page = () => {
-  return <MainLayout>page</MainLayout>;
-};
-
-export default page;
--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import MainLayout from './layout/MainLayout';
-
-export default function Home() {
-  return <MainLayout>hello world</MainLayout>;
-}
