c84df271f14553e4c7430ae5ba0b7e7c1f13fb92
Taiki0807 <hosokawataiki265@gmail.com>
2023-05-28 00:34:13 +0900

    feat: create wordbook list component

--- app/_components/pages/WordlearningPage/WordlearningPage.tsx
+++ app/_components/pages/WordlearningPage/WordlearningPage.tsx
@@ -1,15 +1,23 @@
-'use client';
-import { WordCard } from '../../features';
-import { Button } from '../../parts';
 import style from './WordlearningPage.module.css';
+import Card from './components/Card/Card';
+import { getCourses } from './getCourses';
 
-export const WordlearningPage = (): JSX.Element => {
+export const WordlearningPage = async () => {
+  const { Data } = await getCourses();
+  console.log(Data);
   return (
-    <div className={style.wordlearningPage}>
-      <WordCard />
-      <div className={style.btn}>
-        <Button color="primary">ok</Button>
-        <Button color="primary">ok</Button>
+    <div className={style.learning__page}>
+      <div className={style.courses__wrapper}>
+        <h5>単語一覧</h5>
+        <div className={style.card_items}>
+          {Data.map((item) => (
+            <Card
+              key={item.id}
+              id={item.id}
+              name={item.name}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
