3b05c283aa29a5865f1eda3aac101ab0a8436c12
Taiki0807 <hosokawataiki265@gmail.com>
2023-05-28 23:07:47 +0900

    feat: add modal and control buttons

--- app/_components/pages/WordPage/WordPage.tsx
+++ app/_components/pages/WordPage/WordPage.tsx
@@ -1,21 +1,26 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import { useRecoilValue } from 'recoil';
+import { useState } from 'react';
+import {
+  FaRegArrowAltCircleLeft,
+  FaRegArrowAltCircleRight,
+} from 'react-icons/fa';
 import { WordCard } from '../../features';
-import { Button } from '../../parts';
-import { wordsIDState } from '../WordlearningPage/wordsID';
+import { Button, Modal } from '../../parts';
 import style from './WordPage.module.css';
+import { useLocalStorage } from '@/hooks/useLocalStorage';
 
 interface Props {
   id: string;
 }
 
 export const WordPage = ({ id }: Props) => {
-  const wordsID = useRecoilValue(wordsIDState);
+  const [wordsID] = useLocalStorage('wordsID', ['']);
+
+  const [courseID] = useLocalStorage('courseID', '');
   const router = useRouter();
-  if (!wordsID.length) {
-    router.push('/wordlearning');
-  }
+  const [modal, setModal] = useState(false);
+  const [response, setResponse] = useState(0);
   const findIndexById = (id: string) => {
     const index = wordsID.findIndex(
       (item: string) => item === id
@@ -24,11 +29,36 @@ export const WordPage = ({ id }: Props) => {
   };
 
   const Index = findIndexById(id);
-  const nextWord = () => {
+  const nextWord = async () => {
     if (Index < wordsID.length - 1) {
       const newIndex = Index + 1;
       const currentWordId = wordsID[newIndex];
-      router.push(`/wordlearning/${currentWordId}/`);
+      await router.push(`/wordlearning/${currentWordId}/`);
+    } else {
+      try {
+        const today = new Date()
+          .toISOString()
+          .split('T')[0];
+        const url = `http://localhost:8000/wordbook/reviews/?course_id=${courseID}&review_date=${today}`;
+
+        const response = await fetch(url, {
+          credentials: 'include',
+        });
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        if (courseID && data[courseID]?.length > 0) {
+          setResponse(
+            data[courseID][0].course_progress_rate
+          );
+        }
+        setModal(true);
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
   const prevWord = () => {
@@ -38,17 +68,60 @@ export const WordPage = ({ id }: Props) => {
       router.push(`/wordlearning/${currentWordId}/`);
     }
   };
+  const handleCorrectAnswer = () => {
+    nextWord();
+  };
+  const handleIncorrectAnswer = () => {
+    nextWord();
+  };
+
+  const handleClose_modal = () => {
+    setModal(false);
+  };
 
   return (
     <div className={style.wordlearningPage}>
+      <Modal open={modal} onClose={handleClose_modal}>
+        <div className={style.modal__wrapper}>
+          <p>終了！</p>
+          進捗率：{response * 100}%
+        </div>
+      </Modal>
       <WordCard id={id} />
-      <div className={style.btn}>
-        <Button color="primary" onClick={prevWord}>
-          Previous
-        </Button>
-        <Button color="primary" onClick={nextWord}>
-          Next
-        </Button>
+      <div className={style.card__bottom}>
+        <div className={style.card__control}>
+          <button
+            onClick={prevWord}
+            className={style.button}
+          >
+            <FaRegArrowAltCircleLeft fontSize={25} />
+          </button>
+
+          <p>
+            {Index + 1}/{wordsID.length}
+          </p>
+
+          <button
+            onClick={nextWord}
+            className={style.button}
+          >
+            <FaRegArrowAltCircleRight fontSize={25} />
+          </button>
+        </div>
+        <div className={style.btn}>
+          <Button
+            color="danger"
+            onClick={handleIncorrectAnswer}
+          >
+            不正解
+          </Button>
+          <Button
+            color="primary"
+            onClick={handleCorrectAnswer}
+          >
+            正解
+          </Button>
+        </div>
       </div>
     </div>
   );
