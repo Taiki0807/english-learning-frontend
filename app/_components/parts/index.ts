4dd95c21336b47d3c70e8886841bd57eeca9e7c4
Taiki0807 <hosokawataiki265@gmail.com>
2023-07-01 17:43:01 +0900

    feat: add select box component

--- app/_components/parts/index.ts
+++ app/_components/parts/index.ts
@@ -1,6 +1,7 @@
 export { Button } from './Button';
 export { Popover } from './Popover';
-export {Form} from './Form';
-export {InputField} from './InputField';
-export {Modal} from './Modal';
-export {PieChart} from './PieChart';
+export { Form } from './Form';
+export { InputField } from './InputField';
+export { Modal } from './Modal';
+export { PieChart } from './PieChart';
+export { Select } from './Select';
