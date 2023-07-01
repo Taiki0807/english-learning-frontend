7559b70381f9c31783f53214b98d2dee4f6402a1
Taiki0807 <hosokawataiki265@gmail.com>
2023-07-01 18:01:39 +0900

    feat: add empty data message and fix table key warning

--- app/_components/pages/RegisterPage/components/Table.tsx
+++ app/_components/pages/RegisterPage/components/Table.tsx
@@ -1,11 +1,7 @@
 import style from './Table.module.css';
 
-interface President {
-  Name: string;
-  Index: number;
-}
 interface Props {
-  pres: President[];
+  pres: any[];
 }
 const Table = ({ pres }: Props) => {
   return (
@@ -13,39 +9,52 @@ const Table = ({ pres }: Props) => {
       <div className={style.tbl}>
         {pres.length > 0 && (
           <table>
-            <tr>
-              <th>
-                <input
-                  className={style.check__input}
-                  type="checkbox"
-                />
-              </th>
-              {Object.keys(pres[0]).map((key: string) => (
-                <th key={key}>{key}</th>
-              ))}
+            <thead>
+              <tr>
+                <th>
+                  <input
+                    className={style.check__input}
+                    type="checkbox"
+                  />
+                </th>
+                {Object.keys(pres[0]).map(
+                  (key: string, index: number) => (
+                    <th key={`header-${index}`}>{key}</th>
+                  )
+                )}
 
-              <th>編集</th>
-            </tr>
+                <th>編集</th>
+              </tr>
+            </thead>
             <tbody>
               {pres.map((row, index) => (
-                <tr key={index}>
-                  <td key={index}>
+                <tr key={`row-${index}`}>
+                  <td key={`checkbox-${index}`}>
                     <input
                       className={style.check__input}
                       type="checkbox"
                     />
                   </td>
                   {Object.values(row).map(
-                    (value, index) => (
-                      <td key={index}>{value}</td>
+                    (value: any, columnIndex: number) => (
+                      <td
+                        key={`cell-${index}-${columnIndex}`}
+                      >
+                        {value}
+                      </td>
                     )
                   )}
-                  <td key={index}>編集</td>
+                  <td key={`edit-${index}`}>編集</td>
                 </tr>
               ))}
             </tbody>
           </table>
         )}
+        <p>
+          {pres.length == 0
+            ? '該当するデータがありません'
+            : ''}
+        </p>
       </div>
     </div>
   );
