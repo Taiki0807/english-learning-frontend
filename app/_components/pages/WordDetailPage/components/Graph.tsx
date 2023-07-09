bcdec1d492979b6f7a8ac94e589d32269985e7a2
Taiki0807 <hosokawataiki265@gmail.com>
2023-07-09 21:57:36 +0900

    feat: add Graph component

---
+++ app/_components/pages/WordDetailPage/components/Graph.tsx
@@ -0,0 +1,49 @@
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+} from 'recharts';
+import { UserData } from '../type';
+import {
+  calculateExponential,
+  calculateElapsedTimes,
+} from '@/utils/utils';
+interface Props {
+  previewData: UserData;
+  id: string;
+}
+export const Graph = ({ previewData, id }: Props) => {
+  const targetData = previewData[id];
+  console.log(
+    'retention_rates',
+    targetData[0].user_learning_data.review_dates
+  );
+
+  const elapsedTimes = calculateElapsedTimes(
+    targetData[0].user_learning_data.review_dates
+  );
+  console.log('time', elapsedTimes);
+  const { data } = calculateExponential(
+    elapsedTimes,
+    targetData[0].user_learning_data.retention_rates
+  );
+
+  return (
+    <div>
+      <LineChart width={600} height={300} data={data}>
+        <Line
+          type="monotone"
+          dataKey="retentionRates"
+          stroke="#8884d8"
+          strokeWidth={3}
+          dot={false}
+        />
+        <CartesianGrid stroke="#ddd" />
+        <XAxis dataKey="day" />
+        <YAxis />
+      </LineChart>
+    </div>
+  );
+};
