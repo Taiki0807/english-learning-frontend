97b5489e40dddd74e58c81cc740d6313a1bec5b2
Taiki0807 <hosokawataiki265@gmail.com>
2023-07-18 02:36:26 +0900

    fix: change directory for authentication files

--- app/_components/pages/index.ts
+++ app/_components/pages/index.ts
@@ -1,6 +1,7 @@
 export { Header } from './Header';
-export { SignUp } from './SignUp';
+export { SignInPage } from './SignInPage';
 export { SideBar } from './SideBar';
 export { WordlearningPage } from './WordlearningPage';
 export { WordDetailPage } from './WordDetailPage';
 export { ProfilePage } from './ProfilePage';
+export { SignUpPage } from './SignUpPage';
