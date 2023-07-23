f09f307b62202189c5a9b891738f4a5cb07cd34f
Taiki0807 <hosokawataiki265@gmail.com>
2023-07-23 19:20:23 +0900

    feat: add AbortButton to the page

--- app/_components/pages/WordPage/WordPage.tsx
+++ app/_components/pages/WordPage/WordPage.tsx
@@ -8,6 +8,7 @@ import {
 import { WordCard } from '../../features';
 import { Button, Modal, PieChart } from '../../parts';
 import style from './WordPage.module.css';
+import { AbortButton } from './components';
 import { useAuthContext } from '@/app/_components/features/Auth/SignIn/AuthContext';
 import { useLocalStorage } from '@/hooks/useLocalStorage';
 import {
@@ -166,6 +167,7 @@ export const WordPage = ({ id }: Props) => {
 
   return (
     <div className={style.wordlearningPage}>
+      <AbortButton className={style.abortButton} />
       <Modal open={modal} onClose={handleCloseModal}>
         <div className={style.modal__wrapper}>
           <h1>結果🎉</h1>
