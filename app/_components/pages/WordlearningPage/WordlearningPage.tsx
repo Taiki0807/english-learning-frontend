33cf3b7436a7b74a25e0fb3f7304f646a9d60178
Taiki0807 <hosokawataiki265@gmail.com>
2023-07-23 13:45:31 +0900

    refactor: organize wordbook list using CSS grid layout

--- app/_components/pages/WordlearningPage/WordlearningPage.tsx
+++ app/_components/pages/WordlearningPage/WordlearningPage.tsx
@@ -3,16 +3,20 @@ import { Carousel } from '../../parts';
 import style from './WordlearningPage.module.css';
 import Card from './components/Card/Card';
 import { useGetCourses } from './useGetCourses';
+import { useGetRecentlyAccessedCourses } from './useGetRecentlyAccessedCourses';
 
 export const WordlearningPage = () => {
   const { data } = useGetCourses();
+  const { accessedCourses } =
+    useGetRecentlyAccessedCourses();
   if (!data) return <div>Loading...</div>;
+  if (!accessedCourses) return <div>Loading...</div>;
   return (
     <div className={style.learning__page}>
       <div className={style.courses__wrapper}>
         <h5>最近アクセスした単語帳一覧</h5>
         <div className={style.card_items}>
-          <Carousel cards={data} />
+          <Carousel cards={accessedCourses} />
         </div>
       </div>
       <h5 className={style.title}>単語一覧</h5>
