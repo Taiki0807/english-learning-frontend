ebff6ec58c9fe7441a9f1a18a2f97fe4fcf6bd74
Taiki0807 <hosokawataiki265@gmail.com>
2023-07-25 06:32:32 +0900

    feat: add about page

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,5 +1,11 @@
+import { About } from '@/app/_components/pages/About';
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <About />
+    </div>
+  );
 };
 
 export default page;
